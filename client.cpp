4c4b7b0fb489530666a14cb567dfc1581b04d15d
budiebeizi <41565302+budiebeizi@users.noreply.github.com>
2021-11-25 23:07:08 +0800

    Add files via upload

---
+++ client.cpp
@@ -0,0 +1,63 @@
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>
+#include<iostream>
+#include<string.h>
+
+#define SERVER_ADDRESS "127.0.0.1"
+#define SERVER_PORT    3000
+#define SEND_DATA      "hello world!"
+#define recv_Buf_Size  32
+
+int main(int argc,char *argv[])
+{
+        //创建一个socket
+        int clientfd=socket(AF_INET,SOCK_STREAM,0);
+        if(clientfd == -1)
+        {
+                std::cout<<"create client fd error. "<< std::endl;
+                return -1;
+        }
+
+        //连接服务器
+        struct sockaddr_in serveraddr;
+        serveraddr.sin_family = AF_INET;
+        serveraddr.sin_addr.s_addr = inet_addr(SERVER_ADDRESS);
+        serveraddr.sin_port = htons(SERVER_PORT);
+        if(connect(clientfd,(struct sockaddr *)&serveraddr,sizeof(serveraddr))==-1)
+        {
+                std::cout << "connect socket error" << std::endl;
+                return -1;
+        }
+
+        //向服务器端发送数据
+
+        int ret = send(clientfd, SEND_DATA, strlen(SEND_DATA), 0);
+        if(ret != strlen(SEND_DATA))
+        {
+                std::cout << "send data error." << std::endl;
+                return -1;
+        }
+
+        std::cout << "send data successfully, data :" <<SEND_DATA << std::endl;
+
+
+        //客户端收数据
+        char recvBuf[recv_Buf_Size] = {0};
+        ret = recv(clientfd, recvBuf, recv_Buf_Size, 0);
+        if(ret >0)
+        {
+                std::cout << "recv data successfully, data: " << recvBuf <<std::endl;
+
+        }
+        else
+        {
+                std::cout << "recv data error, data: " << recvBuf <<std::endl;
+
+        }
+        //关闭socket
+        close(clientfd);
+
+        return 0;
+}
